fab5dce0dbdff1e300e176a22d755bc075d0651b
MelvinKhakabo <melvinkhakabo@gmail.com>
2024-11-14 03:13:33 +0300

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from backend.routes import *
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
